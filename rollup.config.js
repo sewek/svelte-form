939182a13b9ab7fad42c03cabfc07077e6a9fd1f
Seweryn Malczewski <seweryn.malczewski@gmail.com>
2022-03-20 12:50:58 +0100

    change rollup config format from en to es

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ const pkg = require("./package.json");
 export default {
   input: "src/Form.svelte",
   output: [
-    { file: pkg.module, format: "en" },
+    { file: pkg.module, format: "es" },
     { file: pkg.main, format: "umd", name: "Form" },
   ],
   plugins: [svelte(), resolve()],
